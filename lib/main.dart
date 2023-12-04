4a6d2e61bce356eee7c688d271cf867e141cb79e
Yiğit TİLKİ <bestnemesis1234@gmail.com>
2023-12-05 01:38:53 +0300

    firebase initialized

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,14 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:take_data_and_update_project/app_widget.dart';
+import 'package:take_data_and_update_project/firebase_options.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   await ScreenUtil.ensureScreenSize();
   runApp(AppWidget());
 }
